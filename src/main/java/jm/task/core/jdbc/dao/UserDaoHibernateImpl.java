e977253215855f06eb7a720d767ef25472842424
Paticoid <usolkin.dima@yandex.ru>
2022-09-30 21:59:25 +0300

    решение задачи через Hibernate

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,6 +1,11 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.tool.hbm2ddl.SchemaExport;
 
 import java.util.List;
 
@@ -12,31 +17,116 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-
+        SessionFactory sessionFactory = new Configuration().setProperties(Util.getProperties()).addAnnotatedClass(User.class).addProperties(Util.getProperties()).buildSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+        try {
+            session.beginTransaction();
+            session.createSQLQuery("CREATE TABLE IF NOT EXISTS User(id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,name VARCHAR(100) NOT NULL ,lastname VARCHAR(100) NOT NULL,age TINYINT NOT NULL)").executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+            throw new RuntimeException(e);
+        }
+        finally {
+            sessionFactory.close();
+        }
     }
 
     @Override
     public void dropUsersTable() {
-
+        SessionFactory sessionFactory = new Configuration().setProperties(Util.getProperties()).addAnnotatedClass(User.class).buildSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+        try {
+            session.beginTransaction();
+            session.createSQLQuery("DROP TABLE IF EXISTS User").executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+            throw new RuntimeException(e);
+        }
+        finally {
+            sessionFactory.close();
+        }
     }
+
+
+
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
+        SessionFactory sessionFactory = new Configuration().setProperties(Util.getProperties()).addAnnotatedClass(User.class).buildSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+        try {
+            session.beginTransaction();
+            User user = new User(name, lastName, age);
+            session.save(user);
+            session.getTransaction().commit();
+        }catch (Exception e) {
+            session.getTransaction().rollback();
+            throw new RuntimeException(e);
+
+        }finally{
+            sessionFactory.close();
+
+        }
 
     }
 
     @Override
     public void removeUserById(long id) {
+        SessionFactory sessionFactory = new Configuration().setProperties(Util.getProperties()).addAnnotatedClass(User.class).buildSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+        try {
+            session.beginTransaction();
+            User user = session.get(User.class,id);
+            session.remove(user);
+            session.getTransaction().commit();
+        }catch (Exception e) {
+            session.getTransaction().rollback();
+            throw new RuntimeException(e);
+
+        }finally{
+            sessionFactory.close();
+
+        }
+
 
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        SessionFactory sessionFactory = new Configuration().setProperties(Util.getProperties()).addAnnotatedClass(User.class).buildSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+        try {
+            session.beginTransaction();
+            List <User> all = session.createQuery("FROM User").getResultList();
+            session.getTransaction().commit();
+            return all;
+        }catch (Exception e) {
+            session.getTransaction().rollback();
+            throw new RuntimeException(e);
+
+        }finally {
+            sessionFactory.close();
+        }
+
     }
 
     @Override
     public void cleanUsersTable() {
+        SessionFactory sessionFactory = new Configuration().setProperties(Util.getProperties()).addAnnotatedClass(User.class).buildSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+        try {
+            session.beginTransaction();
+            session.createQuery("DELETE User").executeUpdate();
+            session.getTransaction().commit();
+        }catch (Exception e) {
+            session.getTransaction().rollback();
+            throw new RuntimeException(e);
+
+        }finally {
+            sessionFactory.close();
+        }
 
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,9 +1,12 @@
 package jm.task.core.jdbc.util;
 
+import org.hibernate.cfg.Environment;
+
 import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
     private static final String URL = "jdbc:mysql://localhost:3306/pre-project";
@@ -27,6 +30,17 @@ public class Util {
             Connection conn = DriverManager.getConnection(URL, userName, password);
             conn.setAutoCommit(false);
             return conn;
+        }
+        public static Properties getProperties(){
+            Properties properties = new Properties();
+            properties.setProperty(Environment.URL,URL);
+            properties.setProperty(Environment.DRIVER,"com.mysql.cj.jdbc.Driver");
+            properties.setProperty(Environment.USER,userName);
+            properties.setProperty(Environment.PASS,password);
+            properties.setProperty(Environment.CURRENT_SESSION_CONTEXT_CLASS,"thread");
+            properties.setProperty(Environment.DIALECT,"org.hibernate.dialect.MySQLDialect");
+            properties.setProperty(Environment.SHOW_SQL,"true");
+            return properties;
         }
 
 
